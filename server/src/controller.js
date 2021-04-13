fb8136182869015c67aa24fdda2e5a588999eb74
Erico-Muller <ericomuller0@gmail.com>
2021-04-13 08:04:02 -0300

    commit 13.4.2021.1

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,1 +1,47 @@
-//nothing yet
+export default class Controller {
+
+    #users = new Map()
+
+    constructor({ socketServer }){
+        this.socketServer = socketServer
+    }
+
+    onNewConnection(socket){
+
+        const { id } = socket
+
+        console.log('connection stablished with', id)
+
+        const userData = { id, socket }
+
+        this.#updateGlobalUserData(id, userData)
+
+        socket.on('data', this.#onSocketData(id))
+        //socket.on('error')
+        //socket.on('end')
+
+    }
+
+    #onSocketData(id){
+
+
+
+    }
+
+    #updateGlobalUserData(socketId, userData){
+
+        const users = this.#users
+        const user = users.get(socketId) ?? {}
+
+        const updateUserData = {
+            ...user,
+            ...userData
+        }
+
+        this.#users.set(socketId, updateUserData)
+
+        return users.get(socketId)
+
+    }
+
+}
